b99d34b3a2bd5c97ab476ab0f1481179f67ed22d
Ephrem-Adugna <ephremadugna1219@gmail.com>
2021-05-31 22:24:09 -0700

    Initial Commit

---
+++ FunctionApp1/LoggingService.cs
@@ -0,0 +1,35 @@
+﻿using logginglib;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+namespace FunctionApp1
+{
+    public class LoggingService
+    {
+        public Logger Init()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var logger = serviceProvider.GetService<Logger>();
+            return logger;
+        }
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddLogging(config =>
+            {
+                config.AddConsole();
+            })
+               .Configure<LoggerFilterOptions>(options =>
+               {
+                   options.AddFilter<ConsoleLoggerProvider>(null, LogLevel.Debug);
+               })
+               .AddTransient<Logger>();
+        }
+    }
+
+}
+
